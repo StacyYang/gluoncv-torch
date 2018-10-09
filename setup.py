30a2d9ef98aa3fb20d9d1820616a9b00ae4526da
Zhang <zhang.hang@rutgers.edu>
2018-10-09 15:42:48 -0700

    init

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import os
+import io
+import re
+from setuptools import setup, find_packages
+import setuptools.command.develop 
+import setuptools.command.install 
+
+version = '0.0.0'
+
+try:
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], 
+        cwd=cwd).decode('ascii').strip()
+    version += '+' + sha[:7]
+except Exception:
+    pass
+
+def create_version_file():
+    global version, cwd
+    print('-- Building version ' + version)
+    version_path = os.path.join(cwd, 'encoding', 'version.py')
+    with open(version_path, 'w') as f:
+        f.write('"""This is encoding version file."""\n')
+        f.write("__version__ = '{}'\n".format(version))
+
+# run test scrip after installation
+class install(setuptools.command.install.install):
+    def run(self):
+        create_version_file()
+        setuptools.command.install.install.run(self)
+
+class develop(setuptools.command.develop.develop):
+    def run(self):
+        create_version_file()
+        setuptools.command.develop.develop.run(self)
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+requirements = [
+    'numpy',
+]
+
+setup(
+    # Metadata
+    name='gluoncv-torch',
+    version=version,
+    author='Gluon CV Toolkit Contributors',
+    url='https://github.com/dmlc/gluon-cv',
+    description='MXNet Gluon CV Toolkit',
+    long_description=long_description,
+    license='Apache-2.0',
+
+    # Package info
+    packages=find_packages(exclude=('docs', 'tests', 'scripts')),
+
+    zip_safe=True,
+    include_package_data=True,
+    install_requires=requirements,
+)
